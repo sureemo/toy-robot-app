cbadcc29e9ac74bbbc5d4ae106aa84d8bf530ea4
Mo Sureerat <sureemo@gmail.com>
2024-10-17 15:53:46 +0700

    Add prompt to accept input from user

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
-export function greet(): string {
-  return 'Hello World';
+import * as readline from 'node:readline';
+
+/**
+ * Init prompt for accept the input from the user
+ */
+function initializeReadline(): void {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  console.log('Move Toy Robot on 5x5 Table');
+  console.log('Enter commands (PLACE x,y,DIRECTION | MOVE | LEFT | RIGHT | REPORT):');
+  console.log('(Available DIRECTION: NORTH, EAST, SOUTH, WEST)');
+
+  rl.on('line', (input) => {
+    console.log(input);
+  });
 }
 
-console.log(greet());
+initializeReadline();
--- tests/app.test.ts
+++
@@ -1,7 +0,0 @@
-import { greet } from '../src/app';
-
-describe('Greet Tests', () => {
-  test('should return Hello World', () => {
-    expect(greet()).toEqual('Hello World');
-  });
-});
